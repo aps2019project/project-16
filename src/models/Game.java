1d8dafc5858dfddc260d78938b1c89dc3acaf11f
Mostafa <m.ojaghi79@gmail.com>
2019-04-21 20:01:17 +0430

    Game class created

---
+++ src/models/Game.java
@@ -0,0 +1,27 @@
+package models;
+
+public abstract class Game {
+    private static final int NUMBER_OF_PLAYERS = 2;
+    private Account[] accounts = new Account[NUMBER_OF_PLAYERS];
+    private Player[] players = new Player[NUMBER_OF_PLAYERS];
+    private GameMap map = new GameMap();
+    private int turn = 0;
+
+    protected Game(Account firstAccount, Account secondAccount, int reward) {
+
+    }
+
+    public void startTurn() {
+
+    }
+
+    public void endTurn() {
+
+    }
+
+    private void setMana() {
+
+    }
+
+    protected abstract boolean checkWinningCondition();
+}
