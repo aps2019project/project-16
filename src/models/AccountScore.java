be55b76235a5d8dbeef016b78fde29825555f11a
sepehrMSP <sepehrpourghannad@yahoo.com>
2019-04-22 23:01:04 +0430

    getters added to AccountScore class

--- src/models/AccountScore.java
+++ src/models/AccountScore.java
@@ -4,6 +4,14 @@ public class AccountScore {
     private int accountName;
     private int wins;
 
+    public int getAccountName() {
+        return accountName;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
     public void incrementWins() {
         wins++;
     }
