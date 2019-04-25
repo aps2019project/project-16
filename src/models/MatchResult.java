bd28cd534b5d5a36f9a4df7d895143ae4f9628b4
sepehrMSP <sepehrpourghannad@yahoo.com>
2019-04-25 20:06:49 +0430

    MatchResult Class added to Model

---
+++ src/models/MatchResult.java
@@ -0,0 +1,40 @@
+package models;
+
+import java.util.Date;
+
+public class MatchResult {
+    private String opponentName;
+    private boolean win ;
+    private Date matchTime ;
+
+    public String getOpponentName() {
+        return opponentName;
+    }
+
+    public void setOpponentName(String opponentName) {
+        this.opponentName = opponentName;
+    }
+
+    public boolean isWin() {
+        return win;
+    }
+
+    public void setWin(boolean win) {
+        this.win = win;
+    }
+
+    public Date getMatchTime() {
+        return matchTime;
+    }
+
+    public void setMatchTime(Date matchTime) {
+        this.matchTime = matchTime;
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public String toString() {
+        return null;
+        //////todo must be implemented
+    }
+}
