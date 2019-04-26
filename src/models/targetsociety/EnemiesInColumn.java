c5d4d94b131c0ffa54f277d13c17817318bb45f0
Mostafa <m.ojaghi79@gmail.com>
2019-04-26 16:27:41 +0430

    EnemiesInColumn targetSociety class added.

---
+++ src/models/targetsociety/EnemiesInColumn.java
@@ -0,0 +1,29 @@
+package models.targetsociety;
+
+import models.Buff;
+import models.Cell;
+import models.Player;
+import models.Table;
+
+import java.util.ArrayList;
+
+public class EnemiesInColumn extends TargetSociety {
+    @Override
+    public boolean canCast(Player player, Cell cell) {
+        for (int column = 0; column < Table.HEIGHT; column++) {
+            Cell cellInComlumn = cell.getTable().getCell(cell.getRow(), column);
+            if (cellInComlumn.hasUnit() && cellInComlumn.getUnit().getPlayer() != player)
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void cast(Player player, Cell cell, ArrayList<Buff> buffs) {
+        for (int column = 0; column < Table.HEIGHT; column++) {
+            Cell cellInColumn = cell.getTable().getCell(cell.getRow(), column);
+            if (cellInColumn.hasUnit() && cellInColumn.getUnit().getPlayer() != player)
+                cellInColumn.getUnit().addBuffs(buffs);
+        }
+    }
+}
