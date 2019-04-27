34d6c53da32b5ecba13a2f1d127f673c4ec45b98
Mostafa <m.ojaghi79@gmail.com>
2019-04-28 02:58:43 +0430

    Range added to attack type constructors.

--- src/models/Hybrid.java
+++ src/models/Hybrid.java
@@ -3,6 +3,10 @@ package models;
 public class Hybrid extends AttackType {
     private int range;
 
+    public Hybrid(int range) {
+        this.range = range;
+    }
+
     @Override
     public boolean canAttack(Cell myCell, Cell oppCell) {
         return Table.getDistance(myCell,oppCell) <= range;
--- src/models/Ranged.java
+++ src/models/Ranged.java
@@ -3,8 +3,12 @@ package models;
 public class Ranged extends AttackType {
     private int range;
 
+    public Ranged(int range) {
+        this.range = range;
+    }
+
     @Override
     public boolean canAttack(Cell myCell, Cell oppCell) {
-        return Table.getDistance(myCell,oppCell) <= range && ! Table.isAdjacent(myCell, oppCell);
+        return Table.getDistance(myCell, oppCell) <= range && !Table.isAdjacent(myCell, oppCell);
     }
 }
