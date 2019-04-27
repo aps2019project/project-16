fe5172806657de792fe3afe7f87a1e4c2b251c6b
Mostafa <m.ojaghi79@gmail.com>
2019-04-27 23:12:18 +0430

    some target societies added.

---
+++ src/models/targetsociety/OneRandomEnemyMinion.java
@@ -0,0 +1,43 @@
+package models.targetsociety;
+
+import models.Buff;
+import models.Cell;
+import models.Player;
+import models.Table;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class OneRandomEnemyMinion extends TargetSociety {
+    @Override
+    public boolean canCast(Player player, Cell cell) {
+        Table table = cell.getTable();
+        for (int i = 0; i < Table.HEIGHT; i++)
+            for (int j = 0; j < Table.WIDTH; j++)
+                if (table.getCell(i, j).hasUnit() && table.getCell(i, j).getUnit().getPlayer() != player)
+                    return true;
+        return false;
+    }
+
+    @Override
+    public void cast(Player player, Cell cell, ArrayList<Buff> buffs) {
+        if (!canCast(player, cell))
+            return;
+        Table table = cell.getTable();
+        Random random = new Random();
+        int randomRow = random.nextInt(Table.HEIGHT);
+        int randomColumn = random.nextInt(Table.WIDTH);
+        int row = randomRow;
+        do {
+            int column = randomColumn;
+            do {
+                if (table.getCell(row, column).hasUnit() && table.getCell(row, column).getUnit().getPlayer() != player) {
+                    table.getCell(row, column).getUnit().addBuffs(buffs);
+                    return;
+                }
+                column = (column + 1) % Table.WIDTH;
+            } while (column != randomColumn);
+            row = (row + 1) % Table.HEIGHT;
+        } while(row != randomRow);
+    }
+}
