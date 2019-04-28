d2431d7f6055e3e6a4850896f0544a852143e5ea
Mostafa <m.ojaghi79@gmail.com>
2019-04-29 02:10:06 +0430

    canCast method of EnemyMinions Adjacent to Cell rewrote.

--- src/models/targetsociety/EnemyMinionsAdjacentToCell.java
+++ src/models/targetsociety/EnemyMinionsAdjacentToCell.java
@@ -11,12 +11,12 @@ import java.util.Random;
 public class EnemyMinionsAdjacentToCell extends TargetSociety {
     @Override
     public boolean canCast(Player player, Cell cell) {
-        Table table = cell.getTable();
-        for (int i = 0; i < Table.HEIGHT; i++)
-            for (int j = 0; j < Table.WIDTH; j++)
-                if (Table.isAdjacent(cell, table.getCell(i, j)) && table.getCell(i, j).hasUnit()
-                        && table.getCell(i, j).getUnit().getPlayer() != player)
-                    return true;
+        int[][] cells = {{0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
+        for (int[] coordinate : cells) {
+            Cell adjacentCell = cell.getTable().getCell(cell.getRow() + coordinate[0], cell.getColumn() + coordinate[1]);
+            if (adjacentCell.hasUnit() && adjacentCell.getUnit().getPlayer() != player)
+                return true;
+        }
         return false;
     }
 
