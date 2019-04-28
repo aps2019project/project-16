c84fb579b63bc6ab1c25148b6e08af1c58f57311
Mostafa <m.ojaghi79@gmail.com>
2019-04-29 02:51:31 +0430

    OneUnit class changed and know extends UnitTargetSociety. OneEnemyUnit and OneFriendUnit deleted.

--- src/models/targetsociety/OneUnit.java
+++ src/models/targetsociety/OneUnit.java
@@ -6,14 +6,19 @@ import models.Player;
 
 import java.util.ArrayList;
 
-public class OneUnit extends TargetSociety {
+public class OneUnit extends UnitTargetSociety {
+    public OneUnit(TargetType targetType, TargetTeam targetTeam) {
+        super(targetType, targetTeam);
+    }
+
     @Override
     public boolean canCast(Player player, Cell cell) {
-        return cell.hasUnit();
+        return cell.hasUnit() && doesEffect(cell.getUnit(), player);
     }
 
     @Override
     public void cast(Player player, Cell cell, ArrayList<Buff> buffs) {
-        cell.getUnit().addBuffs(buffs);
+        if (canCast(player, cell))
+            cell.getUnit().addBuffs(buffs);
     }
 }
