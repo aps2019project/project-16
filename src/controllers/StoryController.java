e5822c7cf16cb1a7d7e4ddb11008a36eff6aa2f8
sadeghsalimi <ssalimi@ce.sharif.edu>
2019-04-30 18:54:41 +0430

    Story commands added and implemented.

--- src/controllers/StoryController.java
+++ src/controllers/StoryController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import contracts.StoryContract;
+import models.GameContents;
 import view.views.StoryView;
 
 public class StoryController implements StoryContract.Controller {
@@ -11,15 +12,14 @@ public class StoryController implements StoryContract.Controller {
         view.setController(this);
     }
 
-    // TODO: 4/30/19 implemennt funcs
-
     @Override
     public void loadLevel(int levelNumber) {
-
+        // TODO: 4/30/19 first must make a game by level properties
+        view.goToLevelInGame();
     }
 
     @Override
     public void loadAllLevels() {
-
+        view.showAllLevels(GameContents.getGameLevels());
     }
 }
---
+++ src/view/commands/storyCommands/EnterLevelCommand.java
@@ -0,0 +1,24 @@
+package view.commands.storyCommands;
+
+import controllers.StoryController;
+import view.Notify;
+import view.commands.Command;
+
+import java.util.regex.Pattern;
+
+public class EnterLevelCommand extends Command {
+    {
+        name = "enter level (level number)";
+        pattern = Pattern.compile("enter level (\\d+)", Pattern.CASE_INSENSITIVE);
+    }
+
+    @Override
+    public void doIt() {
+        int levelNumber = Integer.parseInt(matcher.group(1));
+        if (levelNumber > 3 || levelNumber < 1) {
+            Notify.logError("OOPS! Level number isn't valid!");
+        } else {
+            new StoryController().loadLevel(levelNumber);
+        }
+    }
+}
