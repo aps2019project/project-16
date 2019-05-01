8bf096bae28d75c5c000382b3a4f96b794244537
sadeghsalimi <ssalimi@ce.sharif.edu>
2019-05-02 01:13:12 +0430

    GameCardId generator implemented + class renamed

---
+++ src/models/UniqueIDGenerator.java
@@ -0,0 +1,45 @@
+package models;
+
+import java.util.ArrayList;
+
+public class UniqueIDGenerator {
+    private static int lastUsedID = 0;
+    private static ArrayList<Wrapper> wrappers = new ArrayList<>();
+
+    public static int getCollectionUniqueID() {
+        lastUsedID++;
+        return lastUsedID;
+    }
+
+    public static int getGameUniqueID(String playerName, String cardName) {
+        for (Wrapper wrapper : wrappers) {
+            if (wrapper.equals(playerName, cardName)) {
+                return wrapper.getNewNumberOfCard();
+            }
+        }
+        Wrapper newWrapper = new Wrapper(playerName, cardName);
+        wrappers.add(newWrapper);
+        return newWrapper.getNewNumberOfCard();
+    }
+
+    private static class Wrapper {
+        private String playerName;
+        private String cardName;
+        private int lastNumberOfCard = 0;
+
+        Wrapper(String playerName, String cardName) {
+            this.playerName = playerName;
+            this.cardName = cardName;
+        }
+
+        int getNewNumberOfCard() {
+            lastNumberOfCard++;
+            return lastNumberOfCard;
+        }
+
+        public boolean equals(String playerName, String cardName) {
+            return playerName.equals(this.playerName) && cardName.equals(this.cardName);
+        }
+    }
+
+}
