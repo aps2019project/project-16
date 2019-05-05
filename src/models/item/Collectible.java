3c23b01d481d6032abc189cb04e3f461c20762f1
Mostafa <m.ojaghi79@gmail.com>
2019-05-05 18:41:19 +0430

    error fix

--- src/models/item/Collectible.java
+++ src/models/item/Collectible.java
@@ -1,7 +1,15 @@
 package models.item;
 
+import models.Cell;
+import models.Player;
+
 public class Collectible extends Item{
-    public void use() {
+    public Collectible(String name, int buyPrice, int sellPrice, Type type) {
+        super(name, buyPrice, sellPrice, type);
+    }
+
+    @Override
+    public void use(Player player, Cell cell) {
 
     }
 }
--- src/models/item/Usable.java
+++ src/models/item/Usable.java
@@ -1,7 +1,15 @@
 package models.item;
 
+import models.Cell;
+import models.Player;
+
 public class Usable extends Item {
-    public void use() {
+    public Usable(String name, int buyPrice, int sellPrice, Type type) {
+        super(name, buyPrice, sellPrice, type);
+    }
+
+    @Override
+    public void use(Player player, Cell cell) {
 
     }
 }
