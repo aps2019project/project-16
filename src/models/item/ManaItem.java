b25cac8629a7439d6041c2b75419028776689f8d
Mostafa <m.ojaghi79@gmail.com>
2019-05-05 17:48:54 +0430

    ManaItem builder added.

--- src/models/item/ManaItem.java
+++ src/models/item/ManaItem.java
@@ -7,15 +7,36 @@ public class ManaItem extends Item {
     private int mana;
     private int duration;
 
-    public ManaItem(int mana, int duration) {
+    private ManaItem(String name, int buyPrice, int sellPrice, int mana, int duration) {
+        super(name, buyPrice, sellPrice);
         this.mana = mana;
         this.duration = duration;
+    }
+
+    public static class Builder extends Item.Builder {
+        private int mana;
+        private int duration;
+
+        public Builder setMana(int mana) {
+            this.mana = mana;
+            return this;
+        }
+
+        public Builder setDuration(int duration) {
+            this.duration = duration;
+            return this;
+        }
+
+        @Override
+        public Item create() {
+            return new ManaItem(getName(), getBuyPrice(), getSellPrice(), mana, duration);
+        }
     }
 
     @Override
     public void use(Player player, Cell cell) { //remember to cast on own turn only.
         if (duration > 0)
             player.setMana(player.getMana() + mana);
-        duration --;
+        duration--;
     }
 }
