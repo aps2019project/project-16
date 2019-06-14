45cd58738687d579a0dea9f8aab15529fb917d46
sadeghsalimi <ssalimi@ce.sharif.edu>
2019-06-14 15:07:14 +0430

    I changed map Tiles to polygon

--- src/newView/GraphicalElements/battle/Tile.java
+++ src/newView/GraphicalElements/battle/Tile.java
@@ -1,52 +1,69 @@
 package newView.GraphicalElements.battle;
 
-import javafx.scene.image.Image;
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import newView.GraphicalElements.ScaleTool;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 public class Tile extends Pane {
-    private Image normalImage = new Image(new FileInputStream("src/newView/resources/tiles/normal.png"));
-    private Image hoverImage = new Image(new FileInputStream("src/newView/resources/tiles/hover.png"));
-    private Image selectedImage = new Image(new FileInputStream("src/newView/resources/tiles/selected.png"));
+    private int row;
+    private int column;
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
     private boolean isSelected = false;
-    private ImageView bgView = new ImageView(normalImage);
+    private TilePolygon polygon = new TilePolygon();
     private ImageView imageView;
+    public static final double NORMAL_OPACITY = 0.2;
+    public static final double HOVER_OPACITY = 0.4;
+    public static final double SELECTED_OPACITY = 0.6;
     public static final double TILE_LENGTH = 80;
 
-    public Tile() throws FileNotFoundException {
-        ScaleTool.resizeImageView(bgView, TILE_LENGTH, TILE_LENGTH);
-        this.getChildren().add(bgView);
-        this.setOnMouseEntered(event -> {
-            if (!isSelected) {
-                bgView.setImage(hoverImage);
-            }
-        });
-        this.setOnMouseExited(event -> {
-            if (!isSelected) {
-                bgView.setImage(normalImage);
-            }
-        });
-        this.setOnMouseClicked(event -> {
-            isSelected = !isSelected;
-            if (isSelected) {
-                bgView.setImage(selectedImage);
-            } else {
-                bgView.setImage(hoverImage);
-            }
-        });
+    public Tile(int row, int column) {
+        this.row = row;
+        this.column = column;
+        ScaleTool.relocateTile(this);
+        ScaleTool.makeTilePoints(polygon, row, column);
+        this.getChildren().add(polygon);
+        setMouseEventsFor(polygon);
     }
 
     public void setImageView(ImageView imageView) {
         if (this.imageView != null) {
-            this.imageView.setImage(imageView.getImage());
-        } else {
-            this.imageView = imageView;
+            this.getChildren().remove(this.imageView);
+        }
+        this.imageView = imageView;
+        if (imageView != null) {
             ScaleTool.resizeImageView(imageView, TILE_LENGTH, TILE_LENGTH);
             this.getChildren().add(imageView);
+            setMouseEventsFor(imageView);
         }
+    }
+
+    private void setMouseEventsFor(Node node) {
+        node.setOnMouseEntered(event -> {
+            if (!isSelected) {
+                polygon.setOpacity(HOVER_OPACITY);
+            }
+        });
+        node.setOnMouseExited(event -> {
+            if (!isSelected) {
+                polygon.setOpacity(NORMAL_OPACITY);
+            }
+        });
+        node.setOnMouseClicked(event -> {
+            isSelected = !isSelected;
+            if (isSelected) {
+                polygon.setOpacity(SELECTED_OPACITY);
+            } else {
+                polygon.setOpacity(HOVER_OPACITY);
+            }
+        });
     }
 }
