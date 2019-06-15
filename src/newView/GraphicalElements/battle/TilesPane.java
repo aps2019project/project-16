91ab8136caeda00e6d7364b2bef464a059923ad7
sadeghsalimi <ssalimi@ce.sharif.edu>
2019-06-15 16:46:26 +0430

    can change tiles size

--- src/newView/GraphicalElements/battle/TilesPane.java
+++ src/newView/GraphicalElements/battle/TilesPane.java
@@ -9,17 +9,21 @@ import static newView.SceneMakers.SceneMaker.WIDTH;
 
 public class TilesPane extends AnchorPane {
     private Tile[][] tiles;
+    private final static int NUMBER_OF_ROWS = 5;
+    private final static int NUMBER_OF_COLUMNS = 9;
 
     public TilesPane() {
         super();
-        ScaleTool.relocate(this, WIDTH / 2 - TILE_LENGTH * 5, HEIGHT / 2 - TILE_LENGTH * 3);
+        ScaleTool.relocate(this
+                , WIDTH / 2 - TILE_LENGTH * ((int)(NUMBER_OF_COLUMNS / 2) + 1)
+                , HEIGHT / 2 - TILE_LENGTH * ((int)(NUMBER_OF_ROWS / 2) + 1));
         initTiles();
     }
 
     private void initTiles() {
-        tiles = new Tile[5][9];
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 9; j++) {
+        tiles = new Tile[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS];
+        for (int i = 0; i < NUMBER_OF_ROWS; i++) {
+            for (int j = 0; j < NUMBER_OF_COLUMNS; j++) {
                 Tile tile = new Tile(i, j);
                 tiles[i][j] = tile;
                 this.getChildren().add(tile);
