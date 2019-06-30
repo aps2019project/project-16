a8ce206ed25013fb6298e8791240031ab425644a
sadeghsalimi <ssalimi@ce.sharif.edu>
2019-06-30 15:52:38 +0430

    Sound playing: added todo for Sepehr

---
+++ src/newView/SoundPlayer.java
@@ -0,0 +1,12 @@
+package newView;
+
+public class SoundPlayer {
+    public static void playByPath(String relativePath) {
+        // TODO Sepehr
+    }
+
+    public static void playCardNameSound(String name, Type type) {
+        // TODO Sepehr
+        //  type mitoone "spell" ya "minion" ya "hero" bashe
+    }
+}
