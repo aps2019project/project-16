0b8e03c6482ceb411877882ddb94c7cf9704879e
sepehrMSP <sepehrpourghannad@yahoo.com>
2019-07-02 16:52:39 +0430

    get loading pane method added

--- src/newView/SceneMakers/LoadingSceneMaker.java
+++ src/newView/SceneMakers/LoadingSceneMaker.java
@@ -1,5 +1,6 @@
 package newView.SceneMakers;
 
+import com.dd.plist.PropertyListFormatException;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -11,8 +12,12 @@ import javafx.stage.Stage;
 import newView.AnimationMaker;
 import newView.GraphicalElements.MyScene;
 import newView.GraphicalElements.ScaleTool;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
 
 public class LoadingSceneMaker extends SceneMaker {
 
@@ -22,9 +27,20 @@ public class LoadingSceneMaker extends SceneMaker {
 
     @Override
     public Scene makeScene() throws Exception {
+        Pane pane = getLoading();
+        new Thread(() -> {
+            try {
+                Thread.sleep(2500);
+                Platform.runLater(() -> new LoginSceneMaker(getPrimaryStage()).set());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+        return new MyScene(pane);
+    }
+
+    public Pane getLoading() throws Exception {
         Pane pane = new Pane();
-
-
         ImageView demon = AnimationMaker.getSimpleAnimation("demon", "src/newView/resources/loading/");
         ImageView brand = new ImageView(new Image(new FileInputStream("src/newView/resources/loading/brand_duelyst_preloading.png")));
         Text text = new Text();
@@ -42,14 +58,7 @@ public class LoadingSceneMaker extends SceneMaker {
         pane.getChildren().add(demon);
         pane.getChildren().add(brand);
         pane.setStyle("-fx-background-color: rgb(0,0,0); -fx-text-fill: White");
-        new Thread(() -> {
-            try {
-                Thread.sleep(2500);
-                Platform.runLater(() -> new LoginSceneMaker(getPrimaryStage()).set());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
-        return new MyScene(pane);
+        return pane;
     }
+
 }
