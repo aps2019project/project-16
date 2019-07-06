5cc5010f77eb1463442a0fe996b6bda443e16d72
MostafaOjaghi <m.ojaghi79@gmail.com>
2019-07-07 01:21:43 +0430

    LogoutRequest created.

---
+++ src/models/net/requests/LogoutRequest.java
@@ -0,0 +1,10 @@
+package models.net.requests;
+
+import models.net.RequestPacket;
+
+public class LogoutRequest extends RequestPacket {
+    @Override
+    public void run() {
+        // TODO mostafa: 7/7/19
+    }
+}
