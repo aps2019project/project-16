80723ff7ea497f65aa6c5b291d9edebc944bb376
sepehrMSP <sepehrpourghannad@yahoo.com>
2019-07-06 23:37:14 +0430

    back added to score board

--- src/newView/SceneMakers/ScoreBoardSceneMaker.java
+++ src/newView/SceneMakers/ScoreBoardSceneMaker.java
@@ -1,10 +1,15 @@
 package newView.SceneMakers;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import newView.GraphicalElements.BackgroundMaker;
 import newView.GraphicalElements.MyScene;
+import newView.GraphicalElements.ScaleTool;
+
+import java.io.FileInputStream;
 
 public class ScoreBoardSceneMaker extends SceneMaker {
     public ScoreBoardSceneMaker(Stage primaryStage) {
@@ -14,9 +19,14 @@ public class ScoreBoardSceneMaker extends SceneMaker {
     @Override
     public Scene makeScene() throws Exception {
         Pane pane = new Pane();
-        BackgroundMaker.setBackgroundFor(pane , 1 , "scoreBoard");
+        BackgroundMaker.setBackgroundFor(pane, 1, "scoreBoard");
+
+        ImageView back = new ImageView(new Image(new FileInputStream("src/newView/resources/scoreBoard/back.png")));
+        ScaleTool.resizeImageView(back, 85, 85);
+        back.setOnMouseClicked(event -> new MainMenuSceneMaker(getPrimaryStage()).set());
 
 
+        pane.getChildren().add(back);
         return new MyScene(pane);
     }
 }
